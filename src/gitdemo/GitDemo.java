249d3be12e13caaf0cbb9ab285753f2e674027a7
Martonnvok <martonsom@gmail.com>
2023-09-20 12:56:45 +0200

    no ciklus

--- src/gitdemo/GitDemo.java
+++ src/gitdemo/GitDemo.java
@@ -1,11 +1,23 @@
+
 package gitdemo;
 
 
 public class GitDemo {
 
-    
+
     public static void main(String[] args) {
-       
+        metodus();
+        for (int i = 0; i < 10; i++) {
+            if (true) {
+                
+            }else{
+            
+            }
+        }
+    }
+
+    private static void metodus() {
+        System.out.println("wwwwwwwwwwww");
     }
     
 }
